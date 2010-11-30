828cafccd1320c2abd57f14dc6325aaf64a75da9
Kevin LaCoste <klacoste@zenvilla.com>
2010-11-30 14:37:25 +0900

    Private Methods: Replaced private convenience method.

--- source/RMSSamplePlugin+Sandwich.m
+++ source/RMSSamplePlugin+Sandwich.m
@@ -19,46 +19,46 @@
 
 + (void)load
 {
-	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	
 	[RMSandwich setFactoryClass:self forType:@"RapidWeaver HTML Code Data"];
 	
 	[pool drain];
 }
 
-+ (id)createWithSandwich:(RMSandwich*)sandwich
++ (id)createWithSandwich:(RMSandwich *)sandwich
 {
-	RMSSamplePlugin* plugin = [[[RMSSamplePlugin alloc] init] autorelease];
+	RMSSamplePlugin *plugin = [[[RMSSamplePlugin alloc] init] autorelease];
 	
-	NSDictionary* dictionary = [sandwich sandwichFillingForVersion:0].dictionary;
-	NSDictionary* files = [sandwich sandwichFillingForVersion:0].files;
+	NSDictionary *dictionary = [sandwich sandwichFillingForVersion:0].dictionary;
+	NSDictionary *files = [sandwich sandwichFillingForVersion:0].files;
 	
-	NSString* pathToHTMLContents = [files objectForKey:@"Contents.html"];
-	if(pathToHTMLContents)
+	NSString *pathToHTMLContents = [files objectForKey:@"Contents.html"];
+	if (pathToHTMLContents)
 	{
 		plugin.content = [[NSString stringWithContentsOfFile:pathToHTMLContents encoding:NSUTF8StringEncoding error:NULL] retain];
 	}
 	
-	NSNumber* applyThemeToPageNumber = [dictionary objectForKey:@"ApplyThemeToPage"];
-	if(applyThemeToPageNumber) [plugin setEmitRawContent:(![applyThemeToPageNumber boolValue])];
+	NSNumber *applyThemeToPageNumber = [dictionary objectForKey:@"ApplyThemeToPage"];
+	if (applyThemeToPageNumber) [plugin setEmitRawContent:(![applyThemeToPageNumber boolValue])];
 	
 	return plugin;
 }
 
-- (RMSandwich*)sandwich
+- (RMSandwich *)sandwich
 {
-	RMSandwich* sandwich = [RMSandwich sandwichWithType:@"RapidWeaver HTML Code Data"];
+	RMSandwich *sandwich = [RMSandwich sandwichWithType:@"RapidWeaver HTML Code Data"];
 	
-	NSDictionary* dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+	NSDictionary *dictionary = [NSDictionary dictionaryWithObjectsAndKeys:
 		[NSNumber numberWithBool:(self.emitRawContent == NO)], @"ApplyThemeToPage", nil];
 	[sandwich setDictionary:dictionary forVersion:0];
 	
-	NSString* pathToHTMLContents = [TheFileManager() temporaryFilenameWithPrefix:@"HTMLPageSave" extension:@"html"];
+	NSString *pathToHTMLContents = [[NSFileManager defaultManager] temporaryFilenameWithPrefix:@"HTMLPageSave" extension:@"html"];
 	
 	NSString *html = (contentViewController.content) ?: self.content;
 	[html writeToFile:pathToHTMLContents atomically:NO encoding:NSUTF8StringEncoding error:NULL];
 	
-	NSDictionary* files = [NSDictionary dictionaryWithObjectsAndKeys:pathToHTMLContents, @"Contents.html", nil];
+	NSDictionary *files = [NSDictionary dictionaryWithObjectsAndKeys:pathToHTMLContents, @"Contents.html", nil];
 	[sandwich setFiles:files forVersion:0];
 	
 	return sandwich;
