e5c65ec0a4516ed4034a6ad8e5671bbc8333d56a
Tom Bradley <tom@71squared.com>
2018-08-02 16:04:39 +0100

    Updated RMKit & RWKit

--- Frameworks/RWKit.framework/Versions/A/Headers/RWAppSettings.h
+++ Frameworks/RWKit.framework/Versions/A/Headers/RWAppSettings.h
@@ -12,7 +12,25 @@
 
 + (RWAppSettings *)sharedAppSettings;
 
+// Retrieve a value for the specified key from User Defaults.
+// Using this method is preferable over [NSUserDefaults standardUserDefaults] as it also searches old versions of RapidWeaver
 - (id)valueForKey:(NSString *)key;
-- (void)setValue:(id)value forKey:(NSString *)key;
+
+// Please migrate to an application group container to store your preferences.
+// You'll need to add an App Group in your entitlements file. Then you can access preferences using the following code.
+// The advantage of this approach is that you're free to change your bundle id while retaining access to your preferences. Also, the
+// same preferences easily are accessable from different versions of RapidWeaver.
+
+// Set a value
+/*
+NSUserDefaults *appGroupDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"[DEVELOPERID].com.domain.appgroupname"];
+[appGroupDefaults setValue:@"myValue" forKey:@"myKey"];
+[appGroupDefaults synchronize];
+*/
+
+// Get a value
+/*
+[appGroupDefaults valueForKey:@"myKey"];
+*/
 
 @end
