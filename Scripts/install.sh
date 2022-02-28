7f260f0d9c99341483ad60f932597d3ebab0ca4c
Tom Bradley <tom@71squared.com>
2022-02-28 13:14:31 +0000

    Modernise project settings and allow builds on Apple Silicon

--- Scripts/install.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-if [-e "$HOME/Library/Application Support/RapidWeaver/${PRODUCT_NAME}.rwplugin"]; then
-    rm -rf "$HOME/Library/Application Support/RapidWeaver/${PRODUCT_NAME}.rwplugin"
-fi
-
-ln -sf "${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.rwplugin" "$HOME/Library/Application Support/RapidWeaver"
-
-exit 0;
